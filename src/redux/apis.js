2bc61701a778499565e42ef4a1d0b9437e37d958
Reshin P <reshin.pulapaddy@cesltd.com>
2022-09-06 15:39:08 +0530

    completed

--- src/redux/apis.js
+++ src/redux/apis.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const YOUR_APP_ID = "7af510dd";
-const YOUR_APP_KEY = "5bed27adb25c63c40f8427f943d0c61d";
+const YOUR_APP_ID = "";
+const YOUR_APP_KEY = "";
 
 export const getRecipes = async (query) => {
   const url = `https://api.edamam.com/search?q=${query}&app_id=${YOUR_APP_ID}&app_key=${YOUR_APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free`;
